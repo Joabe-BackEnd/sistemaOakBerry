a513f330f091fbfda067ab21246f236327ce2649
Joabe-BackEnd <joabe.4njos@gmail.com>
2026-04-17 18:31:47 -0300

    nenhum teste feito até agora

---
+++ src/test/java/com/piratasmall/oakberry/sistema/SistemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piratasmall.oakberry.sistema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
